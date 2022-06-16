22d9301394ad40fd3887e455989e02fa21ccbc79
Zahra Kharaghani <95737269+zkharaghani@users.noreply.github.com>
2022-06-16 16:06:28 +0430

    Update calc_mean_value.py

--- calc_mean_value.py
+++ calc_mean_value.py
@@ -23,3 +23,20 @@ print(meanValue)
 # print(inp.isdigit())
 
 
+sumOfValues = 0
+counter = 0
+while True:
+    inp = input("Enter an integer: ")
+    if inp.lower() == 'q' or inp.lower() == 'quit':
+        break
+    if not inp.isdigit():
+        continue
+    else:
+        sumOfValues += float(inp)
+        counter += 1
+if counter:
+    meanValue = sumOfValues / counter
+    print(f'The mean of numbers is equal to {meanValue:.2f}')
+else:
+    meanValue = None
+print(meanValue)
